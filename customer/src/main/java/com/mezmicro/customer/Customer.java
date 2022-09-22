ea9695e7fb8eec9cd9043061068370d5d91a6bb4
mezez <ekemammezez@gmail.com>
2022-09-22 20:21:37 +0100

    initial commit

---
+++ customer/src/main/java/com/mezmicro/customer/Customer.java
@@ -0,0 +1,24 @@
+package com.mezmicro.customer;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data //lombok
+@Builder //lombok
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+    @Id
+    @SequenceGenerator(name = "customer_id_sequence", sequenceName = "customer_id_sequence")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_id_sequence")
+
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
---
+++ customer/src/main/java/com/mezmicro/customer/CustomerRegistrationRequest.java
@@ -0,0 +1,4 @@
+package com.mezmicro.customer;
+
+public record CustomerRegistrationRequest(String firstName, String lastName, String email) {
+}
